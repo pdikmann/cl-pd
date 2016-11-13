46b4398520860f847326b8dc37575b3239ce10f9
Philipp Dikmann <mail@philippdikmann.de>
2016-11-13 21:50:58 +0100

    closer to a macro abstraction

--- main.lisp
+++ main.lisp
@@ -7,6 +7,9 @@
 (defparameter *nodes* nil)
 (defparameter *connections* nil)
 
+;; --------------------------------------------------------------------------------
+;; data
+;; 
 (defstruct node
   (name)                  ; string representation of object
   (init-args)             ; string representation of initial arguments
@@ -21,6 +24,16 @@
   (in-port)                            ; index of input port
   )
 
+(defstruct port
+  (number)
+  (node))
+
+;; --------------------------------------------------------------------------------
+;; pd
+;; 
+(defun to-string (any)
+  (format nil "~s" any))
+
 (defun write-node (n)
   (princ (concatenate 'string
                       "#X obj 0 0 "
@@ -30,12 +43,12 @@
                       (string #\newline))
          nil))
 
-(defun to-string (num)
-  (format nil "~d" num))
-
 (defun write-connection (c)
   (let ((out (find (connection-out-id c) *nodes* :key #'node-id))
         (in (find (connection-in-id c) *nodes* :key #'node-id)))
+    (cond ((or (null out)
+               (null in))
+           (error "wow, no node found!")))
     (princ (concatenate 'string
                         "#X connect "
                         (to-string (node-index out)) " "
@@ -57,6 +70,33 @@
               *connections*)
       ))
 
+
+(defun process-node-args (n args)
+  (let ((port 0))
+    (mapcar (lambda (arg)
+              (cond
+                ((node-p arg)                    ; connect node
+                 (add-connection (node-id arg) 0 ; default output port
+                                 (node-id n) port) ; argument position
+                 (incf port))
+                ((port-p arg)
+                 (add-connection (node-id (port-node arg)) (port-number arg)
+                                 (node-id n) port)
+                 (incf port))
+                ((not (null arg))       ; add init-arg literal
+                 (setf (node-init-args n)
+                       (concatenate 'string
+                                    (node-init-args n) " "
+                                    (to-string arg))))
+                ((null arg)
+                 (incf port)            ; an argument of `nil` explicitly increments the port counter
+                 )))
+            args))
+  n)
+
+;; --------------------------------------------------------------------------------
+;; user-facing
+;; 
 (defun add-connection (out-id out-port in-id in-port)
   (let ((c (make-connection :out-id out-id
                             :out-port out-port
@@ -77,8 +117,12 @@
     ,@form
     (write-patch)))
 
+(defun port (number node)
+  (make-port :number number
+             :node node))
+
 ;; --------------------------------------------------------------------------------
-;; nodes
+;; node library
 ;;
 (defun osc~ (freq)
   (let ((n (make-node :name "osc~")))
@@ -91,34 +135,56 @@
     (add-node n)
     n))
 
-(defun +~ (&optional a b)
+(defun +~ (&rest args)
   (let ((n (make-node :name "+~")))
-    (mapcar (lambda (arg i)
-              (if (node-p arg)
-                  ;; connect node
-                  (add-connection (node-id arg) 0
-                                  (node-id n) i)
-                  ;; add literal to init-args
-                  (setf (node-init-args n)
-                        (concatenate 'string
-                                     (node-init-args n) " "
-                                     (to-string arg)))))
-            (list a b) ; args
-            (list 0 1) ; input ports
-            )
+    ;; improve: init literals and connections should use independent port indices
+    (process-node-args n args)
     (add-node n)
     n))
 
-(defun dac~ () nil)
+(defun dac~ (&rest args)
+  (let ((n (make-node :name "dac~")))
+    (process-node-args n args)
+    (add-node n)
+    n))
+
+(defun trigger (&rest stuff)
+  (let ((n (make-node :name "t")))
+    (process-node-args n stuff)
+    (add-node n)
+    n))
 
 ;; --------------------------------------------------------------------------------
 ;; usage
 ;;
+
+;; rules:
+;; 
+;; - literals (strings, numbers, symbols) are concatenated verbatim to the object literal.
+;;   (this is how pd initializes objects with arguments)
+;; 
+;; - anything else (nil, nodes, ports) is connected to the input ports of the object.
+;; 
+;; - to connect to a specific input port, you have to put the connectee into the corresponding argument position, e.g. to connect to the second port while skipping the first:
+;;   (+~ nil (osc~ 100))
+;;   and with initialization arguments:
+;;   (+ 10 20 nil (number 20))
+
 (with-patch "test.pd"
   ;; (let ((out (+~ (osc~ 100)
   ;;                (osc~ 200))))
   ;;   (dac~ out
   ;;         out))
-  (+~ 789 (osc~ (osc~ 123)))
+  (dac~ (+~ 100 nil (port 1 (trigger 'b 'b 'b)))
+        (+~ 789 (osc~ (osc~ 123))))
   (+~ 123)
   )
+
+
+;; (with-patch "test.pd"
+;;   (let ((out (pd +~
+;;                  (pd osc~ 100)
+;;                  (pd osc~ 200))))
+;;     (pd dac~
+;;         out
+;;         out)))
